4eb5a68ac2acfca7fd6f02f2faa4df04a32a6694
remvord <rem-den@yandex.ru>
2015-11-23 11:03:49 +0300

    123

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет друг!");
+            Console.ReadLine();
+
+        }
+    }
+}
